ec55e2ce2cf2aecf383f0a562f62baf8d74768ba
Jens Meinicke <jensmeinicke@gmx.de>
2016-09-20 15:38:46 -0400

    add example main file

--- ObjectSizeMeasure/src/size/TestSize.java
+++ ObjectSizeMeasure/src/size/TestSize.java
@@ -1,29 +1,47 @@
 package size;
 
-
 public class TestSize {
 	
-//	int i = 0;
-	
-	X[][] objectArray = new X[2][2];
-	
-	public TestSize() {
-	}
-
-	
 	public static void main(String[] args) {
-		TestSize o = new TestSize();
-		
-//		System.out.println(ObjectSizeMeasure.getSizeInByte(o));
-		
-		o.objectArray[0][1] = new X();
-		o.objectArray[1][0] = new X();
-		o.objectArray[1][1] = new X();
-		
+		ObjectSizeMeasure.VERBOUS = true;
+		TestObject o = new TestObject();
 		System.out.println(ObjectSizeMeasure.getSizeInByte(o));
+		System.out.println("--------------");
+		System.out.println(ObjectSizeMeasure.getSizeInByte(o.object));
+		System.out.println("--------------");
+		System.out.println(ObjectSizeMeasure.getSizeInByte(o.array));
+		System.out.println("--------------");
+		System.out.println(ObjectSizeMeasure.getSizeInByte(o.integer));
+		System.out.println("--------------");
+		System.out.println(ObjectSizeMeasure.getSizeInByte(o.l));
+		
+		System.out.println("--------------");
+		ObjectSizeMeasure.VERBOUS = false;
+		Integer[] array = new Integer[10];
+		System.out.println("array size:" + ObjectSizeMeasure.getSizeInByte(array));
+		for (int i = 0; i < array.length; i++) {
+			array[i] = Integer.valueOf(i);
+			System.out.println("array size:" + ObjectSizeMeasure.getSizeInByte(array));
+		}
+		for (int i = 0; i < array.length; i++) {
+			array[i] = Integer.valueOf(100);
+			System.out.println("array size:" + ObjectSizeMeasure.getSizeInByte(array));
+		}
+		
 	}
 }
 
-class X {
-	Object i = new Object();
+class TestObject {
+	Object object = new Object();
+	Object[] array = new Object[10];
+	int integer = 1;
+	long l = 10;
+	
+	public TestObject() {
+		for (int i = 0; i < array.length; i++) {
+			array[i] = Integer.valueOf(i);
+		}
+			
+	}
 }
+
